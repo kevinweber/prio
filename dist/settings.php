4cb5ac142baaa7a360ac44ff36baa80208bbdec9
kevinweber <k-iwi@web.de>
2016-03-06 18:25:20 -0800

    Removed dist/ folder

--- dist/settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined('CLIENT_ID'))
-    define('CLIENT_ID', '16551d4c73904985c4f0');
-if (!defined('AUTH_CALLBACK_URL'))
-    define('AUTH_CALLBACK_URL', 'http://localhost/git/prio/src/');
-?>
